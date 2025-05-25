d4d220a054e7359108ce73901dadbe45c64913cc
Stekz42 <159025592+Stekz42@users.noreply.github.com>
2025-05-25 16:28:19 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,9 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Stilvoll Immobilien - Ein Bewertungstool für Immobilien" />
+        <title>Stilvoll Immobilien</title>
       </Head>
       <body>
         <Main />
